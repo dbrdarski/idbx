d4f882ebb57655204520d98cec948aead5b87a00
Dane Brdarski <dane@cytrox.com>
2022-12-08 16:50:27 +0100

    added getter type fix

--- src/getters.js
+++ src/getters.js
@@ -55,7 +55,7 @@ export const generateGetters = (instance, store, methods, type) => {
       )
     },
     activeRevisions () {
-      return getDocuments()
+      return this.getDocuments()
         .filter(item => item.active != null)
         .map(item => records.byId[item.active])
     },
