8ee2abecd5d9ea5aa4c9f1d0ce4e56c88d362e5b
Dane Brdarski <dane.brdarski@gmail.com>
2023-03-31 11:48:30 +0200

    added console.log

--- src/schema.js
+++ src/schema.js
@@ -123,6 +123,7 @@ const createModelSchema = model => {
 export const generateSetters = (instance, store, methods, type, initType) => {
   // const validate = createSchema(initType.call(relations, {}))
   const validate = createModelSchema(initType())
+  console.log({ validate })
   const idbx = globalThis.idbx = globalThis.idbx ?? {}
   store[type].validate = idbx[type] = validate
   methods[type].createDocument = (data, publish) => {
