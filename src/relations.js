5b2350abd2a9c8f03e502a025aa8e71d498e570a
Dane Brdarski <dane.brdarski@gmail.com>
2023-04-24 12:47:11 +0200

    wip

--- src/relations.js
+++ src/relations.js
@@ -156,7 +156,7 @@ export const generateRelations = (context, store, methods, type, typeInit, def) 
   }
 
   const recorder = (set, rel, type, handler, id) => {
-    const connections = allRelationships[type][id].activeDocuments?.[rel]
+    const connections = allRelationships[type].activeDocuments[id]?.[rel]
     connections == null && console.log({ type, id, rel, allRelationships })
     if (connections == null) return // TODO: investigate connections.length
       for (const record of store[type]?.getActiveDocuments(...connections)) {
