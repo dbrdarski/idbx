173624c69ac0cfd0095f9d25db0416b768ec1123
dbrdarski <dane.brdarski@gmail.com>
2023-04-26 09:10:26 +0200

    fixed typo

--- src/iterable.js
+++ src/iterable.js
@@ -62,7 +62,7 @@ export default (store, storeHelpers) => {
     data (fn) {
       const relationships = this.#query.includeHandlers
       const def = relationships?.[0]
-      console.log(defm relationships)
+      console.log(def, relationships)
       const includes = relationships && {}
       const iterator = relationships
         ? include(
